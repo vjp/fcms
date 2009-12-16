c6c9804abbf8fa731a1c232992d84a4bd88cf8a0
vano <vano>
2009-12-16 19:40:09 +0000

    getCookie

--- vcms-htdocs/js/base.js
+++ vcms-htdocs/js/base.js
@@ -6,3 +6,16 @@ function setCookie (name, value, expires, path, domain, secure) {
         ((secure) ? "; secure" : "");
 
 }
+
+
+function getCookie( name ) {
+    var start = document.cookie.indexOf( name + "=" );
+    var len = start + name.length + 1;
+    if ( ( !start ) && ( name != document.cookie.substring( 0, name.length ) ) ) {
+        return null;
+    }
+    if ( start == -1 ) return null;
+    var end = document.cookie.indexOf( ';', len );
+    if ( end == -1 ) end = document.cookie.length;
+    return unescape( document.cookie.substring( len, end ) );
+}
