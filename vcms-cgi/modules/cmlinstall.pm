abd2eda7750ce9fbede544b5ab7db5db460ddfc9
vano <vano>
2009-12-21 21:54:42 +0000

    mce styles enable

--- vcms-cgi/modules/cmlinstall.pm
+++ vcms-cgi/modules/cmlinstall.pm
@@ -1,6 +1,6 @@
 package cmlinstall;
 
-# $Id: cmlinstall.pm,v 1.13 2009-12-21 21:43:28 vano Exp $
+# $Id: cmlinstall.pm,v 1.14 2009-12-21 21:54:42 vano Exp $
 
 BEGIN
 {
@@ -358,11 +358,11 @@ tinyMCE.init({ mode : "specific_textareas",
 editor_selector : "mceEditor",
 convert_urls : false,
 theme : "advanced",
-theme_advanced_styles : "Заголовок 1=header1;Заголовок 2=header2;Заголовок 3=header3",
+theme_advanced_styles : "Заголовок 1=mcheader1;Заголовок 2=mcheader2;Заголовок 3=mcheader3",
 theme_advanced_disable : "image",
 theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
 theme_advanced_buttons3 : "",
-content_css : "/css/admin.css",
+content_css : "/css/mce.css",
 apply_source_formatting: true,
 language : "ru"
 });
