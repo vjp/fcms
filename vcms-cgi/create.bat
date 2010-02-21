5ffd576f1fbf101b48441cc684b5e7496a055624
vano <vano>
2010-02-21 17:04:56 +0000

    join cgi & htdocs

--- vcms-cgi/create.bat
+++ vcms-cgi/create.bat
@@ -7,16 +7,16 @@ del cgi.tar
 
 cd ..\vcms-htdocs
 del html.tar.gz
-7za a html.tar *.htm -r
-7za a html.tar *.gif -r
-7za a html.tar *.jpg -r
-7za a html.tar *.png -r
-7za a html.tar *.gif -r
-7za a html.tar *.css -r
-7za a html.tar *.tgz -r
-7za a html.tar *.swf -r
-7za a html.tar *.js -r
-7za a html.tar .htaccess -r
-7za a html.tar.gz html.tar
+..\vcms-cgi\7za a html.tar *.htm -r
+..\vcms-cgi\7za a html.tar *.gif -r
+..\vcms-cgi\7za a html.tar *.jpg -r
+..\vcms-cgi\7za a html.tar *.png -r
+..\vcms-cgi\7za a html.tar *.gif -r
+..\vcms-cgi\7za a html.tar *.css -r
+..\vcms-cgi\7za a html.tar *.tgz -r
+..\vcms-cgi\7za a html.tar *.swf -r
+..\vcms-cgi\7za a html.tar *.js -r
+..\vcms-cgi\7za a html.tar .htaccess -r
+..\vcms-cgi\7za a ..\vcms-cgi\html.tar.gz html.tar
 del html.tar   
-move html.tar.gz ..\vcms-cgi
+
