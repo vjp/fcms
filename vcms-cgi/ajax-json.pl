b706a326f4087c93707af53d3fe42bc0df418851
vano <vano>
2010-04-06 07:43:39 +0000

    cgiparam override

--- vcms-cgi/ajax-json.pl
+++ vcms-cgi/ajax-json.pl
@@ -17,7 +17,7 @@ my $json = new JSON::PP;
 print "Content-Type: application/json; charset=$GLOBAL->{CODEPAGE}\n\n";
 my $data=param('data') || $json->encode ([]);
 my $func=param('func');
-my $r=decode_json($data);
+$cmlcalc::CGIPARAM=decode_json($data);
 my $result=execute({method=>$func});
 if (ref $result ne 'HASH') {
 	$result=({status=>"execute error. method:$func"});
