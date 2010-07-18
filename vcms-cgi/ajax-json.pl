6a9b7879dc507a8f59a65e333dc4a7cc0d0971e1
vano <vano>
2010-07-18 23:13:10 +0000

    support not unicode sites

--- vcms-cgi/ajax-json.pl
+++ vcms-cgi/ajax-json.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# $Id: ajax-json.pl,v 1.12 2010-06-29 05:59:11 vano Exp $
+# $Id: ajax-json.pl,v 1.13 2010-07-18 23:13:10 vano Exp $
 
 use strict;
 use lib "./modules/";
@@ -20,8 +20,18 @@ print "Content-Type: application/json; charset=$GLOBAL->{CODEPAGE}\n\n";
 my $data=param('data') || $json->encode ([]);
 my $func=param('func');
 check_session();
+
+
 my $prms=decode_json($data);
-$cmlcalc::CGIPARAM=$prms if ref $prms eq 'HASH';
+if (ref $prms eq 'HASH') {
+	unless ($GLOBAL->{CODEPAGE} eq 'utf-8') {
+		$prms->{$_} = Encode::encode('cp1251',$prms->{$_}) for keys %$prms;
+	}	
+	$cmlcalc::CGIPARAM=$prms;
+}	
+
+
+
 $cmlcalc::CGIPARAM->{_MODE}='USERAJAX';
 $cmlcalc::ENV->{SERVER}=$ENV{SERVER_NAME};
 $cmlcalc::ENV->{USER}=$ENV{REMOTE_USER} || '%user';
