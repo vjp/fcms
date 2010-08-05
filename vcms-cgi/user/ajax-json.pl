9d6f5d1c2ab00662241f00c1676abe192f44c14e
vano <vano>
2010-08-05 21:21:44 +0000

    interface like adminajax

---
+++ vcms-cgi/user/ajax-json.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl -w
+
+# $Id: ajax-json.pl,v 1.1 2010-08-05 21:21:44 vano Exp $
+
+use strict;
+no strict "refs";
+
+use lib "../modules/";
+use Data::Dumper;
+use JSON::PP;
+
+
+use cmlmain;
+use cmlcalc;
+use cmlajax;
+use CGI  qw/:standard/;     
+use CGI::Carp qw /fatalsToBrowser/;
+use Encode;
+
+use vars qw ($AJAX_FUNCS);
+   
+   
+$AJAX_FUNCS={
+	execute=>1,
+};   
+
+
+start('..');
+print "Content-Type: application/json; charset=$GLOBAL->{CODEPAGE}\n\n";
+
+my $data=param('data');
+my $func=param('func');
+my $json = new JSON::PP;
+$cmlcalc::ENV->{SERVER}=$ENV{SERVER_NAME};
+$cmlcalc::ENV->{USER}=$ENV{REMOTE_USER} || '%admin';
+$cmlcalc::ENV->{USERID}=&cmlcalc::id("SU_$ENV{REMOTE_USER}");
+if ($AJAX_FUNCS->{$func}) {
+	my $subname="cmlajax::ajax_$func";
+	my $r=decode_json($data);
+	$cmlcalc::CGIPARAM=decode_json($data);
+	$cmlcalc::CGIPARAM->{_MODE}='USERAJAX';
+	my $result=&$subname($r);
+	print $json->encode ($result);
+} else {
+	my $rstr="Íåïğàâèëüíàÿ ôóíêöèÿ $func";
+	print $json->encode ({result=>$rstr});
+}	
+
