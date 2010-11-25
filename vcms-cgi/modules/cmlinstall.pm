2f0adf4b9035add254634cff5017a049227871dd
utenkov.i <utenkov.i@gmail.com>
2010-11-26 00:20:31 +0300

    remove not needed funcs

--- vcms-cgi/modules/cmlinstall.pm
+++ vcms-cgi/modules/cmlinstall.pm
@@ -772,22 +772,6 @@ setvalue({convert=>1,key=>'MCEINIT',pkey=>'PAGETEMPLATE',value=>qq(
         language : "ru" 
     
     });
-    function insertimage (text){ 
-        tinyMCE.execCommand('mceInsertContent', false, ' <img src="'+text+'"/> '); 
-        tinyMCE.execCommand('mceInsertContent', false, ''); 
-    }; 
-    function insertlink (src,name){ 
-        tinyMCE.execCommand('mceInsertContent', false, ' <a href="'+src+'"/>'+name+'</a> '); 
-        tinyMCE.execCommand('mceInsertContent', false, ''); 
-    }; 
-    function toggleEditor(id) {
-        var elm = document.getElementById(id);
-
-        if (tinyMCE.getInstanceById(id) == null)
-            tinyMCE.execCommand('mceAddControl', false, id);
-        else
-            tinyMCE.execCommand('mceRemoveControl', false, id);
-    }
 </script>
 )});
 
