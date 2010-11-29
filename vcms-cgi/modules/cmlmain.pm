609332e06a851ad7c5f13c58753cb04d8ab065d7
utenkov.i <utenkov.i@gmail.com>
2010-11-30 01:36:14 +0300

    forced encoding set

--- vcms-cgi/modules/cmlmain.pm
+++ vcms-cgi/modules/cmlmain.pm
@@ -1174,7 +1174,7 @@ sub init	{
   
  	dbconnect({dbname=>$DBNAME,dbuser=>$DBUSER,dbpassword=>$DBPASSWORD,dbhost=>$DBHOST});
  	
-	 
+	$dbh->do($UTF?"SET NAMES utf8":"SET NAMES cp1251"); 
  	$sthDD=$dbh->prepare("DELETE FROM ${DBPREFIX}vls WHERE objid=? AND pkey=? AND lang=?");
  	$sthUDD=$dbh->prepare("DELETE FROM ${DBPREFIX}uvls WHERE objid=? AND pkey=? AND lang=?");
  	$sthI =$dbh->prepare("REPLACE ${DBPREFIX}vls (objid,pkey,value,upobj,lang) VALUES (?,?,?,?,?)") || die $dbh->errstr;
