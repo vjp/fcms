40c2119d21cb0a5c70a7253f95ba028eb0f6a917
utenkov ivan <utenkov@corp.mail.ru>
2011-05-10 00:20:30 +0400

    user ajaxparser copied from common parser

--- vcms-cgi/user/ajax-json.pl
+++ vcms-cgi/user/ajax-json.pl
@@ -1,12 +1,8 @@
 #!/usr/bin/perl -w
 
-# $Id: ajax-json.pl,v 1.1 2010-08-05 21:21:44 vano Exp $
 
 use strict;
-no strict "refs";
-
 use lib "../modules/";
-use Data::Dumper;
 use JSON::PP;
 
 
@@ -15,34 +11,45 @@ use cmlcalc;
 use cmlajax;
 use CGI  qw/:standard/;     
 use CGI::Carp qw /fatalsToBrowser/;
-use Encode;
-
-use vars qw ($AJAX_FUNCS);
-   
-   
-$AJAX_FUNCS={
-	execute=>1,
-};   
-
 
 start('..');
-print "Content-Type: application/json; charset=$GLOBAL->{CODEPAGE}\n\n";
 
-my $data=param('data');
-my $func=param('func');
 my $json = new JSON::PP;
-$cmlcalc::ENV->{SERVER}=$ENV{HTTP_HOST};
-$cmlcalc::ENV->{USER}=$ENV{REMOTE_USER} || '%admin';
-$cmlcalc::ENV->{USERID}=&cmlcalc::id("SU_$ENV{REMOTE_USER}");
-if ($AJAX_FUNCS->{$func}) {
-	my $subname="cmlajax::ajax_$func";
-	my $r=decode_json($data);
-	$cmlcalc::CGIPARAM=decode_json($data);
-	$cmlcalc::CGIPARAM->{_MODE}='USERAJAX';
-	my $result=&$subname($r);
-	print $json->encode ($result);
-} else {
-	my $rstr="Неправильная функция $func";
-	print $json->encode ({result=>$rstr});
+print "Content-Type: application/json; charset=$GLOBAL->{CODEPAGE}\n\n";
+my $data=param('data') || $json->encode ([]);
+my $func=param('func');
+check_session();
+
+
+my $prms=decode_json($data);
+if (ref $prms eq 'HASH') {
+	unless ($GLOBAL->{CODEPAGE} eq 'utf-8') {
+		$prms->{$_} = Encode::encode('cp1251',$prms->{$_}) for keys %$prms;
+	}	
+	$cmlcalc::CGIPARAM=$prms;
 }	
+
+
+
+$cmlcalc::CGIPARAM->{_MODE}='USERAJAX';
+$cmlcalc::ENV->{SERVER}=$ENV{HTTP_HOST};
+$cmlcalc::ENV->{USER}=$ENV{REMOTE_USER} || '%user';
+$cmlcalc::ENV->{USERID}=&cmlcalc::id("SU_$ENV{REMOTE_USER}");
+
+
+my $result;
+if (param('func')) {
+	$result=execute({method=>param('func')});
+} elsif (param('lfunc')) {
+	$result=execute({lmethod=>param('lfunc'),id=>param('objid')});
+} else {
+	$result=enc('Метод не задан')
+}	
+if (ref $result ne 'HASH') {
+	$result=({
+		status=>0,
+		message=>enc("Ошибка выполнения. Метод: $func Ошибка: ").$result,
+	});
+}
+print $json->encode ($result);
 
