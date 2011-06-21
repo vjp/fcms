7ceddbe79b01e0cc5c7f4a4632259e4f85d9a4b2
vjp <utenkov.i@gmail.com>
2011-06-21 22:58:05 +0400

    valid element fix

--- vcms-cgi/modules/cmlinstall.pm
+++ vcms-cgi/modules/cmlinstall.pm
@@ -887,10 +887,7 @@ setvalue({convert=>1,key=>'MCEINIT',pkey=>'PAGETEMPLATE',value=>qq(
         theme_advanced_toolbar_location : "top", 
         content_css : "/css/mce.css", 
         apply_source_formatting: true,
-        
-        extended_valid_elements : "div[id|style|class]",
-       extended_valid_elements : "img[onload|src|border|onmouseover|onmouseout|title|width|height|alt]",
-        
+        extended_valid_elements : "div[id|style|class],img[onload|src|border|onmouseover|onmouseout|title|width|height|alt]",
         language : "ru" 
     
     });
