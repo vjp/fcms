8a81bcdc958d16e7390e1273b16144ff1d831e7a
vjp <utenkov.i@gmail.com>
2012-07-19 23:57:29 +0400

    GetObject func

--- vcms-cgi/modules/vCMS/Collection.pm
+++ vcms-cgi/modules/vCMS/Collection.pm
@@ -7,12 +7,30 @@ sub GetObjects ($) {
 	return $self->{_list}
 }
 
+sub GetObject ($$) {
+	my ($self,$key)=@_;
 
+	my @l=grep {
+    	if ($key=~/^u?\d+$/) {
+        	$_->ID() eq $key
+        } else {
+        	$_->Key() eq $key
+        }		
+	} @{$self->GetObjects()};
+	return $l[0];
+	
+}
 
 sub Dump ($) {
 	my ($self)=shift;
 	my @l=map {$_->Dump()} @{$self->GetObjects()};
 	return \@l;
+}
+
+
+sub Sync ($$) {
+	my ($self,$data)=@_;
+	$self->Fill();
 }
 
 1;
