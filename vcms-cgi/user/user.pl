e90670c8243f8ca044c76167ba7d201a83f99c1a
vjp <utenkov.i@gmail.com>
2012-12-05 23:28:39 +0400

    not need memcached stat

--- vcms-cgi/user/user.pl
+++ vcms-cgi/user/user.pl
@@ -118,9 +118,9 @@ if ($body) {print $body}
 else       {errorpage()}
 
 my $ts=time()-$ts_start;
-warn sprintf("DBG: END: USER:$cmlcalc::ENV->{USER}  QUERY:$qs TIME:%.3f  INIT:%.3f DBRV:(%.3f:%d) MMRV:(%.3f:%d) DBLT (%.3f:%d) DBBL (%.3f:%d) CL (%.3f:%d) DBBR (%.3f:%d) FP (%.3f:%d) TP (%.3f:%d) IC (%.3f:%d)\n",
+warn sprintf("DBG: END: USER:$cmlcalc::ENV->{USER}  QUERY:$qs TIME:%.3f  INIT:%.3f DBRV:(%.3f:%d) DBLT (%.3f:%d) DBBL (%.3f:%d) CL (%.3f:%d) DBBR (%.3f:%d) FP (%.3f:%d) TP (%.3f:%d) IC (%.3f:%d)\n",
 	$ts,$its,
-	$GLOBAL->{ot},$GLOBAL->{otc},$GLOBAL->{mt},$GLOBAL->{mtc},
+	$GLOBAL->{ot},$GLOBAL->{otc},
 	$cmlcalc::TIMERS->{LOWTREE}->{sec},$cmlcalc::TIMERS->{LOWTREE}->{count},
 	$GLOBAL->{timers}->{bl},$GLOBAL->{timers}->{blc},
 	$cmlcalc::TIMERS->{CHECKLOAD}->{sec},$cmlcalc::TIMERS->{CHECKLOAD}->{count},
