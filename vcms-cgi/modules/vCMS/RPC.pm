e3da472de5d03d92a1c80563950abef690640d6e
vjp <utenkov.i@gmail.com>
2013-03-11 00:51:29 +0400

    utf encoding all values - need for json processing

--- vcms-cgi/modules/vCMS/RPC.pm
+++ vcms-cgi/modules/vCMS/RPC.pm
@@ -35,6 +35,7 @@ sub Execute ($$;$) {
 	my $uri="http://$self->{_host}/gate/_$method";
 	my $r = HTTP::Request -> new  (	POST => $uri );
 	$r->content_type('application/x-www-form-urlencoded');
+	$data->{$_}=Encode::encode('utf8',$data->{$_}) for keys %$data;
 	$r->content("data=".JSON::PP->new->utf8->encode ($data));	
 	$r->authorization_basic( $self->{_username}, $self->{_password} );
 	my $response = $self->{_ua}->request($r); 
