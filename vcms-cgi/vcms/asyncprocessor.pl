4569a654f51ad512b47c1e43478dbd6f93ccd81b
vjp <utenkov.i@gmail.com>
2013-05-04 00:41:49 +0400

    script for async processing need add to crontab

---
+++ vcms-cgi/vcms/asyncprocessor.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+
+
+use Data::Dumper;
+use Time::HiRes qw (time);
+
+
+use lib "../modules/";
+
+use cmlmain;
+use cmlcalc;
+use vCMS;
+
+
+start('..');
+$cmlcalc::ENV->{USER}='%asyncprocessor';
+$cmlcalc::CGIPARAM->{_MODE}='ASYNCPROCESSOR';
+($cmlcalc::ENV->{SERVER}) = ($GLOBAL->{ABSFILEURL}=~m{http://(.+)/data});
+o('AUTOMATE')->e('ASYNCPROCESSOR');
