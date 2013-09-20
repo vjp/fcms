20815c2a6a30c00c05eb52b0a1d6a91321f3a9f2
vjp <utenkov.i@gmail.com>
2013-09-21 00:40:04 +0400

    tr colorexpr attr

--- vcms-cgi/modules/cmlparse.pm
+++ vcms-cgi/modules/cmlparse.pm
@@ -438,7 +438,7 @@ sub tag_td {
 	my $param=$_[0]->{param};
 	my $data=$_[0]->{data};
 	my $inner; %{$inner}=%{$_[0]->{inner}};
-	my $pl=fetchparam(\$param,['th','csv','csvmoney','hidden','hiddenexpr']);
+	my $pl=fetchparam(\$param,['th','csv','csvmoney','hidden','hiddenexpr','colorexpr']);
 	my $id=$inner->{objid};	
 	my $body=cmlparser({data=>$data,inner=>$inner});
 	my $tg=($pl->{th} || $inner->{th})?'th':'td';
@@ -453,6 +453,9 @@ sub tag_td {
 		$clstr='style="display:none"';
 	} elsif ($pl->{hiddenexpr}) {
 		$clstr='style="display:none"' if cmlcalc::calculate({id=>$id,expr=>$pl->{hiddenexpr}})->{value};
+	} elsif ($pl->{colorexpr}) {
+		my $clr= &cmlcalc::calc($id,$pl->{colorexpr});
+		$clstr="style='background-color:$clr'";
 	}
 	return "<$tg $param $clstr>$body</$tg>";
 }
