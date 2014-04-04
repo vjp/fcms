21311c936cb05bf0b15c80a7ff59f5347d0ef148
vjp <utenkov.i@gmail.com>
2014-04-04 23:25:30 +0400

    line ends

--- vcms-cgi/stat.pl
+++ vcms-cgi/stat.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
+
 use strict;
+
 my ($d,$v,$s)=($ENV{REQUEST_URI}=~/d=(\d+).+w=(\d+).+s=(\d+)/);
 warn sprintf("BODY GEN %.3fs DOM READY %.3fs FULL LOAD %.3fs\n",$s/1000,$d/1000,$v/1000);
 print "Status: 301 Moved Permanantly\n";
