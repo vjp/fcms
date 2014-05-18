e9c4e3366bcd338cdfa43144900d6a3462f57edf
vjp <utenkov.i@gmail.com>
2014-05-18 23:05:20 +0400

    more stat

--- vcms-cgi/stat.pl
+++ vcms-cgi/stat.pl
@@ -3,6 +3,8 @@
 use strict;
 
 my ($d,$v,$s)=($ENV{REQUEST_URI}=~/d=(\d+).+w=(\d+).+s=(\d+)/);
-warn sprintf("BODY GEN %.3fs DOM READY %.3fs FULL LOAD %.3fs\n",$s/1000,$d/1000,$v/1000);
+my ($cc,$cn)=($ENV{HTTP_COOKIE}=~/_cc=(\S+);.+_cn=(\S+);/s);
+my $geo="$cc $cn";
+warn sprintf("BODY GEN %.3fs DOM READY %.3fs FULL LOAD %.3fs ",$s/1000,$d/1000,$v/1000)."geo : $geo \n";
 print "Status: 301 Moved Permanantly\n";
 print "Location: http://$ENV{SERVER_NAME}/i/0.gif\n\n";
--- vcms-cgi/vcms/statparser.pl
+++ vcms-cgi/vcms/statparser.pl
@@ -27,22 +27,40 @@ if (param('statfile')) {
  	while (my $buffer=shift @l || <$fh>) {
  		if ($buffer=~/\[\w+ (\w+ \d+) \d\d:\d\d:\d\d (\d{4})\]/) {
  			my $dt="$1 $2"; 
-			if ($buffer=~/BODY GEN ([0-9\.]+)s DOM READY ([0-9\.]+)s FULL LOAD ([0-9\.]+)s/) {
+			if ($buffer=~/BODY GEN ([0-9\.]+)s DOM READY ([0-9\.]+)s FULL LOAD ([0-9\.]+)s, referer: (\S+)/) {
  				$stat->{$dt}->{cnt}++;
  				$stat->{$dt}->{bg}+=$1;
  				$stat->{$dt}->{dr}+=$2;
- 				$stat->{$dt}->{fl}+=$3;	
+ 				$stat->{$dt}->{fl}+=$3;
+ 				
+ 				$stat->{$dt}->{refs}->{$4}->{cnt}++;
+ 				$stat->{$dt}->{refs}->{$4}->{bg}+=$1;
+ 				$stat->{$dt}->{refs}->{$4}->{dr}+=$2;
+ 				$stat->{$dt}->{refs}->{$4}->{fl}+=$3;	
+ 					
  			}	 
  		}
  	}
  	for my $dt (sort keys %$stat) {
 		if ($stat->{$dt}->{cnt}) {
- 			printf "DT $dt CNT: %d  BODY GEN: %.2f  DOM READY: %.2f FULL LOAD %.2f <br/>",
+ 			printf "DT $dt CNT: %d  BODY GEN: %.2f  DOM READY: %.2f FULL LOAD %.2f <br/> <br/>",
  				$stat->{$dt}->{cnt},
  				$stat->{$dt}->{bg}/$stat->{$dt}->{cnt},
  				$stat->{$dt}->{dr}/$stat->{$dt}->{cnt},
- 				$stat->{$dt}->{fl}/$stat->{$dt}->{cnt},
+ 				$stat->{$dt}->{fl}/$stat->{$dt}->{cnt};
  		}	
+ 		my $top;
+ 		for my $ref (sort {$stat->{$dt}->{refs}->{$b}->{cnt}<=>$stat->{$dt}->{refs}->{$a}->{cnt}} keys %{$stat->{$dt}->{refs}}) {
+ 			printf "DT $dt REF $ref CNT: %d  BODY GEN: %.2f  DOM READY: %.2f FULL LOAD %.2f <br/>",
+ 				$stat->{$dt}->{refs}->{$ref}->{cnt},
+ 				$stat->{$dt}->{refs}->{$ref}->{bg}/$stat->{$dt}->{refs}->{$ref}->{cnt},
+ 				$stat->{$dt}->{refs}->{$ref}->{dr}/$stat->{$dt}->{refs}->{$ref}->{cnt},
+ 				$stat->{$dt}->{refs}->{$ref}->{fl}/$stat->{$dt}->{refs}->{$ref}->{cnt};
+ 			last if $top++>10;	
+ 		}
+ 		print "<hr/>"
+ 		
+ 		
  	} 
 }
 print start_form();
