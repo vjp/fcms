00c6cd15c7ca57ef283cc425540e24dcc9fc5d75
vjp <utenkov.i@gmail.com>
2014-05-25 23:34:25 +0400

    geo stat

--- vcms-cgi/stat.pl
+++ vcms-cgi/stat.pl
@@ -1,11 +1,15 @@
 #!/usr/bin/perl
 
 use strict;
+use URI::Escape;
+use Encode;
 
 my ($d,$v,$s,$c)=($ENV{REQUEST_URI}=~/d=(\d+).+w=(\d+).+s=(\d+).+c=([01])/);
-my ($cc)=($ENV{HTTP_COOKIE}=~/_cc=(\S+);/s);
-my ($cn)=($ENV{HTTP_COOKIE}=~/_cn=(\S+);/s);
-my $geo="$cc $cn";
+my ($cc)=($ENV{HTTP_COOKIE}=~/_cc=(\S+?);/s);
+my ($cn)=($ENV{HTTP_COOKIE}=~/_cn=(\S+?);/s);
+my $geo=Encode::encode('cp1251',Encode::decode('utf8',uri_unescape($cc).' '.uri_unescape($cn)));
+
+
 warn sprintf("BODY GEN %.3fs DOM READY %.3fs FULL LOAD %.3fs ",$s/1000,$d/1000,$v/1000)."geo : $geo cached : $c\n";
 print "Status: 301 Moved Permanantly\n";
 print "Location: http://$ENV{SERVER_NAME}/i/0.gif\n\n";
