27d4e80a4415eab480f6bb4352e62025ab880877
ivan utenkov <utenkov.i@gmail.com>
2015-01-08 11:10:13 +0400

    html archive creation fixes

--- vcms-cgi/create.sh
+++ vcms-cgi/create.sh
@@ -1,6 +1,6 @@
 del cgi.tar.gz
 tar -czvf cgi.tar.gz `find -E . -regex '.*(\.pm|\.pl|\.ttf|conf\.template)$'`
-cd ../vcms-htdocs
-del html.tar.gz
-tar -czvf html.tar.gz `find -E . -regex '.*(\.htm|\.gif|\.jpg|\.png|\.css|\.tgz|\.swf|\.js|\.ico|\.htaccess|robots\.txt)$'`
+cd  ../vcms-htdocs
+del ../vcms-cgi/html.tar.gz
+tar -czvf ../vcms-cgi/html.tar.gz `find -E . -regex '.*(\.htm|\.gif|\.jpg|\.png|\.css|\.tgz|\.swf|\.js|\.ico|\.htaccess|robots\.txt)$'`
 
