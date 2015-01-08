07ec800f1e9da774b93a1cea100707cb79b2afde
ivan utenkov <utenkov.i@gmail.com>
2015-01-08 11:07:18 +0400

    install file creation fixes

--- vcms-cgi/create.sh
+++ vcms-cgi/create.sh
@@ -1,6 +1,6 @@
 del cgi.tar.gz
-tar -czvf cgi.tar.gz `find -E . -regex '.*\.(pm|pl|ttf|template)$'`
+tar -czvf cgi.tar.gz `find -E . -regex '.*(\.pm|\.pl|\.ttf|conf\.template)$'`
 cd ../vcms-htdocs
 del html.tar.gz
-tar -czvf html.tar.gz `find -E . -regex '.*\.(htm|gif|jpg|png|css|tgz|swf|js|ico|htaccess|txt)$'`
+tar -czvf html.tar.gz `find -E . -regex '.*(\.htm|\.gif|\.jpg|\.png|\.css|\.tgz|\.swf|\.js|\.ico|\.htaccess|robots\.txt)$'`
 
