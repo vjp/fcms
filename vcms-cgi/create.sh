56161b925579303e8630a06e937c5025aef3e387
ivan utenkov <utenkov.i@gmail.com>
2015-01-25 00:27:21 +0400

    add bootstrap css map

--- vcms-cgi/create.sh
+++ vcms-cgi/create.sh
@@ -2,5 +2,5 @@ rm cgi.tar.gz
 tar -czvf cgi.tar.gz `find -E . -regex '.*(\.pm|\.pl|\.ttf|conf\.template)$'`
 cd  ../vcms-htdocs
 rm ../vcms-cgi/html.tar.gz
-tar -czvf ../vcms-cgi/html.tar.gz `find -E . -regex '.*(\.htm|\.gif|\.jpg|\.png|\.css|\.tgz|\.swf|\.js|\.ico|\.htaccess|robots\.txt)$'`
+tar -czvf ../vcms-cgi/html.tar.gz `find -E . -regex '.*(\.htm|\.gif|\.jpg|\.map|\.png|\.css|\.tgz|\.swf|\.js|\.ico|\.htaccess|robots\.txt)$'`
 
