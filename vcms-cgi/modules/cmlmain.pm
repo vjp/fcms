a05b05459b82fc9cfc283e5eed7cd3b929d12f44
ivan utenkov <utenkov.i@gmail.com>
2015-08-30 23:13:55 +0300

    mysql set names fix

--- vcms-cgi/modules/cmlmain.pm
+++ vcms-cgi/modules/cmlmain.pm
@@ -1560,11 +1560,7 @@ sub init	{
  	$DBPREFIX=$DBPREFIX?"${DBPREFIX}_":'';
   
  	dbconnect({dbname=>$DBNAME,dbuser=>$DBUSER,dbpassword=>$DBPASSWORD,dbhost=>$DBHOST});
- 	if ($UTF) {
- 		$dbh->do("SET NAMES utf8")
- 	} else {
- 		$dbh->do("SET NAMES cp1251")
- 	}
+ 	$dbh->do("SET NAMES $GLOBAL->{ENCODING}");
  	$sthDD=$dbh->prepare("DELETE FROM ${DBPREFIX}vls WHERE objid=? AND pkey=? AND lang=?");
  	$sthUDD=$dbh->prepare("DELETE FROM ${DBPREFIX}uvls WHERE objid=? AND pkey=? AND lang=?");
  	$sthI =$dbh->prepare("REPLACE ${DBPREFIX}vls (objid,pkey,value,upobj,lang) VALUES (?,?,?,?,?)") || die $dbh->errstr;
