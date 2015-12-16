d13159248dca57049da2136717b349c29ff09e25
ivan utenkov <utenkov.i@gmail.com>
2015-12-16 23:20:08 +0300

    add https support to stat script

--- vcms-cgi/stat.pl
+++ vcms-cgi/stat.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
 
-
-
 use strict;
 #use URI::Escape;
 #use Encode;
@@ -14,4 +12,4 @@ my $geo;
 
 warn sprintf("BODY GEN %.3fs DOM READY %.3fs FULL LOAD %.3fs ",$s/1000,$d/1000,$v/1000)."geo : $geo cached : $c\n";
 print "Status: 301 Moved Permanantly\n";
-print "Location: http://$ENV{SERVER_NAME}/i/0.gif\n\n";
+print "Location: $ENV{HTTP_ORIGIN}/i/0.gif\n\n";
