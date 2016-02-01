72cadeeb24a6613989742af7af273493d5d783e2
ivan utenkov <utenkov.i@gmail.com>
2016-02-01 23:23:03 +0300

    auth key for exchange func
    example
    
    vCMS::Currency::Exchange(AU,{debug=>1,key=>$key}),

--- vcms-cgi/modules/vCMS/Currency.pm
+++ vcms-cgi/modules/vCMS/Currency.pm
@@ -22,10 +22,13 @@ sub Quandl ($) {
 sub Exchange ($;$) {
     my ($key,$opts)=@_;
     my $debug;
-    $debug=1 if $opts;
+    $debug=1 if $opts->{debug};
+    my $apikey=$opts->{key};
     my $qkey=Quandl($key);
     if ($qkey) {
-        my $r=vCMS::Proxy::GetURL("https://www.quandl.com/api/v3/datasets/$qkey/data.xml?rows=1&column_index=1");
+        my $url="https://www.quandl.com/api/v3/datasets/$qkey/data.xml?rows=1&column_index=1";
+        $url.="&api_key=$apikey" if $apikey;
+        my $r=vCMS::Proxy::GetURL($url);
         if ($r->{content}) {
             my ($rate)=($r->{content}=~m{<datum type="float">([\d\.]+)</datum>}si);
             return $rate;
