46d4023d18f73b6b8611eb3d08d573ce74b8c304
Ivan Utenkov <utenkov.i@gmail.com>
2017-03-19 22:47:28 +0300

    add datatables.js

--- vcms-cgi/modules/cmlinstall.pm
+++ vcms-cgi/modules/cmlinstall.pm
@@ -370,6 +370,36 @@ setvalue({key=>'INITSCRIPTS',pkey=>'PAGETEMPLATE',convert=>1,value=>qq(
 <script>
   /* window.onerror = jsErrHandlerJq; */
 </script>
+
+
+
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.13/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.13/js/jquery.dataTables.js"></script>
+<script>  
+var dtlang={
+  		"processing": "Подождите...",
+  		"search": "Поиск:",
+  		"lengthMenu": "Показать _MENU_ записей",
+  		"info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+  		"infoEmpty": "Записи с 0 до 0 из 0 записей",
+  		"infoFiltered": "(отфильтровано из _MAX_ записей)",
+  		"infoPostFix": "",
+  		"loadingRecords": "Загрузка записей...",
+  		"zeroRecords": "Записи отсутствуют.",
+  		"emptyTable:": "В таблице отсутствуют данные",
+  		"paginate": {
+    		"first": "Первая",
+    		"previous": "Предыдущая",
+    		"next": "Следующая",
+    		"last": "Последняя"
+  		},
+  		"aria": {
+    		"sortAscending": ": активировать для сортировки столбца по возрастанию",
+    		"sortDescending": ": активировать для сортировки столбца по убыванию"
+  		}
+	}  
+</script>
+
 )});
 
 addlowobject({convertname=>1,upobjkey=>'INCLUDES',key=>'SITEHEADER',name=>'Шапка'});
